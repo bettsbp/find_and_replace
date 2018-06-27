6bd3eb976909df9281177e5a0f88b2d050c57457
Annie Shin and Brandon Betts <brandon@>
2018-06-27 16:25:34 -0700

    fix bug about using only one word

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -7,12 +7,23 @@ class Word < String
   end
 
   def replace_inputted_words
-    new_array = @string.downcase.capitalize.split(" ")
+    new_array = @string.downcase.split(" ")
     new_array.each do |word|
       word.sub! @i1, @i2
     end
-    finished_array = (new_array.join(" ") + ".")
-    print finished_array
+    finished_array = (new_array.join(" ") + ".").capitalize
     finished_array
   end
 end
+
+puts "Enter a sentence:"
+string = gets.chomp
+puts "Enter the word you want to replace:"
+input1 = gets.chomp
+puts "Enter the word to replace it with:"
+input2 = gets.chomp
+new_phrase = Word.new(string, input1, input2)
+puts "Now running..."
+puts "Here is your sentence:"
+final_phrase = new_phrase.replace_inputted_words
+puts final_phrase
